eb5491c3da93213597d8c4fc1e7feed457a0a891
Adylsha Yumayev <68568793+AdylshaY@users.noreply.github.com>
2021-09-21 14:36:20 +0300

    Create ex_09.py

---
+++ ex_09.py
@@ -0,0 +1,7 @@
+for a in range(1, 1000):
+    for b in range (a, 1000):
+        c = 1000-a-b
+        #print(a,b,c)
+        if ((a**2)+(b**2) == (c**2)) and (a+b+c == 1000):
+            print(a,b,c)
+            print(a*b*c)
